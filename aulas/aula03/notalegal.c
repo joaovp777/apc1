9818718ecbcac79f4d8ae2008dda0a14142d96bf
joaovp777 <111434493+joaovp777@users.noreply.github.com>
2023-09-20 13:49:08 +0000

    20/09

---
+++ aulas/aula03/notalegal.c
@@ -0,0 +1,54 @@
+#include <stdio.h> // scanf - ler; printf - escrever;
+
+int main (){
+  char item1[61];
+  int quantidade1;
+  float preco1;
+
+  char item2[61];
+  int quantidade2;
+  float preco2;
+
+  char item3[61];
+  int quantidade3;
+  float preco3;
+
+  printf("entre com o nome do item1: ");
+  scanf("%s", &item1);
+  printf("entre com a qtde do item1: ");
+  scanf("%i", &quantidade1);
+  printf("entre com o preco do item1: ");
+  scanf("%f", &preco1);
+
+  printf("entre com o nome do item2: ");
+  scanf("%s", &item2);
+  printf("entre com a qtde do item2: ");
+  scanf("%i", &quantidade2);
+  printf("entre com o preco do item2: ");
+  scanf("%f", &preco2);
+
+  printf("entre com o nome do item3: ");
+  scanf("%s", &item1);
+  printf("entre com a qtde do item3: ");
+  scanf("%i", &quantidade3);
+  printf("entre com o preco do item3: ");
+  scanf("%f", &preco3);
+  
+  float total = quantidade1 * preco1;
+  total = total + quantidade2 * preco2;
+  total = total + quantidade3 * preco3;
+  
+  printf("--------------------------\n");
+  printf("        NOTA LEGAL        \n");
+  printf("--------------------------\n");
+  printf("Item         qtd Valor\n");
+  printf("%-14s %03i %7.2f\n", item1, quantidade1, preco1);     
+  printf("%-14s %03i %7.2f\n", item2, quantidade2, preco2); 
+  printf("%-14s %03i %7.2f\n", item3, quantidade3, preco3); 
+  printf("--------------------------\n");
+  printf("Total           R$ %7.2f\n", total);
+  printf("--------------------------\n");
+
+
+  return 0;
+}
--- pratica/pratica01/questao01.c
+++ pratica/pratica01/questao01.c
@@ -4,9 +4,15 @@
 
 int main(){
 
-float nota_a1 = 5.5f;
-float nota_a2 = 6.0f;
+  float nota_a1 = 0;
+  float nota_a2 = 0;
 
+  printf("entre com a nota A1 entre 0 e 10: ");
+  scanf("%f", &nota_a1);
+  printf("entre com a nota A2 entre 0 e 10: ");
+  scanf("%f", &nota_a2);
+
+  
 float media = 0.4 * nota_a1 + 0.6 * nota_a2;
 
 printf("a media e %f\n", media);
