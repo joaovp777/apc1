f80ed305c00ffccabcd3c1780a94fa1dcbe70fb7
joaovp777 <111434493+joaovp777@users.noreply.github.com>
2023-09-22 02:15:45 +0000

    21/09

--- pratica/pratica01/questao06.c
+++ pratica/pratica01/questao06.c
@@ -6,15 +6,22 @@
 #include <math.h>
 
 int main (){
-  int coeficiente_a = 1;
-  int coeficiente_b = 3;
-  int coeficiente_c = 2;
+  float coeficiente_a;
+  float coeficiente_b;
+  float coeficiente_c;
+
+  printf("insera o valor do coeficiente a: ");
+  scanf("%f", &coeficiente_a);
+  printf("insera o valor do coeficiente b: ");
+  scanf("%f", &coeficiente_b);
+  printf("insera o valor do coeficiente c: ");
+  scanf("%f", &coeficiente_c);
 
   double delta = pow(coeficiente_b, 2) - 4 * coeficiente_a * coeficiente_c;
   double raiz_x1 = (-coeficiente_b + sqrt(delta)) / 2 * coeficiente_a;
   double raiz_x2 = (-coeficiente_b - sqrt(delta)) / 2 * coeficiente_a;
   
-  printf("as raizes da equacao %ix %x %i sao %f e %f\n",coeficiente_a, coeficiente_c, coeficiente_c,raiz_x1, raiz_x2);
+  printf("as raizes da equacao %f %f %.f sao %f e %f\n",coeficiente_a, coeficiente_c, coeficiente_c,raiz_x1, raiz_x2);
 
   return 0; 
 }
